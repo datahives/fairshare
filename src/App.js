4c58abf907163c978be3a72960cdb0b621b1fd0a
Wattanit Hotrakool <wattanit@eatlab.space>
2018-11-15 15:40:07 +0700

    Add MemberPage and MemberEdit dialog

--- src/App.js
+++ src/App.js
@@ -3,16 +3,52 @@ import logo from './logo.svg'
 import './App.css'
 
 import ButtonAppBar from './AppBar'
+import MemberPage from './MemberPage'
+import MemberEdit from './MemberEdit'
+import ItemPage from './ItemPage'
 
 class App extends Component {
   constructor(props){
     super(props)
 
     this.state = {
-      page: 0
+      page: 1,
+      members: [
+        {
+          name: "Me",
+        },
+      ],
+      items : [
+        {
+          name: "Food 1",
+          price: 65,
+        },
+        {
+          name: "Food 2",
+          price: 45,
+        },
+        {
+          name: "Food 3",
+          price: 100,
+        }
+      ],
+      additions: [
+        {
+          name: "Service charge",
+          value: 0.1
+        },
+        {
+          name: "VAT",
+          value: 0.07
+        }
+      ]
     }
 
     this.handleTapChange = this.handleTapChange.bind(this)
+    this.handleDialogPage = this.handleDialogPage.bind(this)
+    this.handleAddMember = this.handleAddMember.bind(this)
+    this.handleEditMember = this.handleEditMember.bind(this)
+    this.handleDeleteMember = this.handleDeleteMember.bind(this)
   }
 
   handleTapChange = (event, page)=>{
@@ -21,10 +57,66 @@ class App extends Component {
     })
   }
 
+  handleDialogPage = (page, target)=>{
+    this.setState({
+      page: page,
+      target: target
+    })
+  }
+
+  handleAddMember = (member)=>{
+    let members = this.state.members
+    members.push(member)
+    this.setState({
+      page: 0,
+      members: members
+    })
+  }
+
+  handleEditMember = (newmember, oldmember)=>{
+    const idx = this.state.members.indexOf(oldmember)
+    let members = this.state.members
+    members[idx] = newmember
+    this.setState({
+      page: 0,
+      members: members
+    })
+  }
+
+  handleDeleteMember = (oldmember)=>{
+    const idx = this.state.members.indexOf(oldmember)
+    let members = this.state.members
+    members.splice(idx,1)
+    this.setState({
+      page: 0,
+      members: members
+    })
+  }
+
   render() {
+
+    let page
+    switch (this.state.page){
+      case 0: 
+        page = <MemberPage members={this.state.members} handleDialogPage={this.handleDialogPage} handleDeleteMember={this.handleDeleteMember}/> 
+        break
+      case 1:
+        page = <ItemPage items={this.state.items} additions={this.state.additions}/>
+        break
+      case 2:
+        page = <div>Summary page</div>
+        break
+      case 3:
+        page = <MemberEdit target={this.state.target} handleAddMember={this.handleAddMember} handleEditMember={this.handleEditMember}/>
+        break
+      default:
+        page = <div>Default page</div>
+    }
+
     return (
       <div className="App">
         <ButtonAppBar page={this.state.page} handleTapChange={this.handleTapChange}/>
+        {page}
       </div>
     );
   }
---
+++ src/MemberPage.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
+import ListItemText from '@material-ui/core/ListItemText'
+import IconButton from '@material-ui/core/IconButton'
+import Button from '@material-ui/core/Button'
+import CloseIcon from '@material-ui/icons/Close'
+import PersonAddIcon from '@material-ui/icons/PersonAdd'
+
+const styles = (theme) => ({
+    root: {
+        width: '100%',
+        backgroundColor: theme.palette.background.paper
+    },
+    button: {
+        margin: theme.spacing.unit,
+        position: 'absolute',
+        bottom: theme.spacing.unit * 2,
+        right: theme.spacing.unit * 2,
+    }
+})
+
+class MemberList extends Component {
+
+    render(){
+        return (
+            <div className={this.props.classes.root}>
+                <List>
+                    {this.props.members.map((member)=>{
+                        return (
+                            <ListItem key={member.name} button onClick={()=>this.props.handleDialogPage(3, member)}>
+                                <ListItemText primary={member.name}/>
+                                <ListItemSecondaryAction>
+                                    <IconButton aria-label="Delete member" onClick={()=>this.props.handleDeleteMember(member)}>
+                                        <CloseIcon/>
+                                    </IconButton>
+                                </ListItemSecondaryAction>
+                            </ListItem>
+                        )
+                    })}
+                </List>
+                    
+                <Button variant="fab" 
+                    onClick={()=>this.props.handleDialogPage(3, null)}
+                    color="primary" aria-label="Add" 
+                    className={this.props.classes.button}>
+                    <PersonAddIcon/>
+                </Button>
+            </div>
+        )
+    }
+}
+
+MemberList.PropTypes = {
+    classes: PropTypes.object.isRequired,
+}
+
+export default withStyles(styles)(MemberList)
