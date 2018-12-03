de6aa2ee10b4b0dc73199def32c59c13f2bd25d3
Wattanit Hotrakool <wattanit@Wattanits-MacBook-Pro.local>
2018-12-03 21:07:29 +0700

    Add TotalSummaryCard

--- src/SummaryPage.js
+++ src/SummaryPage.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
-import { Button, Card, Elevation, H3, H5 } from '@blueprintjs/core';
+import { Button, Card, Divider, Elevation, H3, H5 } from '@blueprintjs/core';
 import '@blueprintjs/core/lib/css/blueprint.css';
 import './App.css';
 
 import AppBar from './AppBar';
 import BottomBar from './BottomBar';
 import Avatar from './Avatar';
+
+class TotalSummaryCard extends Component{
+    render(){
+        return(
+            <Card className="card" interactive={true} elevation={Elevation.TWO}>
+                <div className="flexTopVertical">
+                    <H5>Total</H5>
+                    <H3>2000 THB</H3>
+                    <Divider/>
+                    <H5>Pot</H5>
+                    <H3>1000 THB</H3>
+                    <Divider/>
+                    <H5>Balance</H5>
+                    <H3>-1000 THB</H3>
+                </div>
+            </Card>
+        );
+    }
+}
 
 class MemberSummaryCard extends Component{
     render(){
@@ -35,6 +54,7 @@ class SummaryPage extends Component {
             <div className="fullview">
                 <AppBar/>
                 <div className="contentpane">
+                    <TotalSummaryCard/>
                     <MemberSummaryCard/>
                     <MemberSummaryCard/>
                 </div>
