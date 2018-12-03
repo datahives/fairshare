8bc577d2964ff1518c1cbb40ad0dfda291c92d75
Wattanit Hotrakool <wattanit@Wattanits-MacBook-Pro.local>
2018-12-03 22:18:32 +0700

    Add AddMember dialog

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 // import { Button } from '@blueprintjs/core';
 
+import Avatar from './Avatar';
 import SplashPage from './SplashPage';
 import MemberPage from './MemberPage';
 import ItemPage from './ItemPage';
 import SummaryPage from './SummaryPage';
+
+const randomcolor = require('randomcolor');
+const sha256 = require('js-sha256');
+
+function getInitial(name){
+  let initials = name.match(/\b\w/g) || [];
+  initials = ((initials.shift() || '') + (initials.pop() || '')).toUpperCase();
+  return initials;
+}
 
 class App extends Component {
 
@@ -13,11 +23,13 @@ class App extends Component {
     super(props);
 
     this.state = {
-      page: 0
+      page: 1,
+      members: []
     };
 
     this.nextPage = this.nextPage.bind(this);
     this.previousPage = this.previousPage.bind(this);
+    this.addMember = this.addMember.bind(this);
   }
 
   nextPage(){
@@ -32,6 +44,19 @@ class App extends Component {
     });
   }
 
+  addMember(name){
+    let members = this.state.members;
+    const newmember = {
+      id: sha256((new Date()).getTime().toString()),
+      name: name,
+      avatar: <Avatar label={getInitial(name)} bgcolor={randomcolor({luminosity: 'light'})} color="black"/>
+    }
+    members.push(newmember);
+    this.setState({
+      members: members,
+    });
+  }
+
   render() {
     let renderedPage;
     switch(this.state.page){
@@ -39,7 +64,11 @@ class App extends Component {
         renderedPage = <SplashPage handleNextPage={this.nextPage}/>;
         break;
       case 1:
-        renderedPage = <MemberPage handleBackPage={this.previousPage} handleNextPage={this.nextPage}/>;
+        renderedPage = <MemberPage 
+          members={this.state.members}
+          handleBackPage={this.previousPage} 
+          handleNextPage={this.nextPage} 
+          handleAddMember={this.addMember}/>;
         break;
       case 2:
         renderedPage = <ItemPage handleBackPage={this.previousPage} handleNextPage={this.nextPage}/>;
@@ -48,7 +77,7 @@ class App extends Component {
         renderedPage = <SummaryPage handleBackPage={this.previousPage} handleNextPage={this.nextPage}/>;
         break;
       default:
-        renderedPage = <div><strong>Four-oh-Four | 404</strong></div>;
+        renderedPage = <div><strong>Four-oh-Four | 404 </strong></div>;
     }
 
     return (
--- src/MemberPage.js
+++ src/MemberPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Card, Elevation, H3 } from '@blueprintjs/core';
+import { Button, Card, Elevation, H3, Icon, Intent, Label,Overlay } from '@blueprintjs/core';
 import '@blueprintjs/core/lib/css/blueprint.css';
 import './App.css';
 
@@ -12,8 +12,8 @@ class MemberCard extends Component {
         return(
           <Card className="card" interactive={true} elevation={Elevation.TWO}>
             <div className="flexLeftHorizontal">
-                <Avatar label="MN" bgcolor="blue" color="white"/>
-                <H3 style={{marginLeft: "20px", lineHeight: "45px"}}>Member name</H3>
+                {this.props.member.avatar}
+                <H3 style={{marginLeft: "20px", lineHeight: "45px"}}>{this.props.member.name}</H3>
             </div>
             <div className="flexRightHorizontal">
                 <Button icon="edit">Edit</Button>
@@ -23,14 +23,83 @@ class MemberCard extends Component {
     }
 }
 
+class NewMemberCard extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            showdialog: false,
+            name: '',
+        };
+
+        this.handleOpen = this.handleOpen.bind(this);
+        this.handleClose = this.handleClose.bind(this);
+        this.onChange = this.onChange.bind(this);
+    }
+
+    handleOpen(){
+        this.setState({
+            showdialog: true,
+        });
+    }
+
+    handleClose(){
+        this.setState({
+            showdialog: false,
+        });
+    }
+
+    onChange(event){
+        this.setState({
+            name: event.target.value,
+        });
+    }
+
+    render(){
+        return(
+            <Card className="card" interactive={true} elevation={Elevation.TWO}>
+                <div className="centerVertical" onClick={this.handleOpen}>
+                    <div className="centerHorizontal">
+                        <Icon icon="plus" iconSize={30}/>
+                    </div>
+                </div>
+                <Overlay isOpen={this.state.showdialog}>
+                    <Card className="dialog" elevation={Elevation.THREE}>
+                        <div className="flexSpanVertical" style={{height: "100%"}}>
+                            <div>
+                                <H3>Add Participant</H3>
+                                <Label>
+                                    Name
+                                    <input className="bp3-input bp3-fill" type="text" placeholder="Name" onChange={this.onChange}/>
+                                </Label>
+                            </div>
+                            <div className="flexRightHorizontal">
+                                <Button intent={Intent.DANGER} fill onClick={this.handleClose}>Cancel</Button>
+                                <Button intent={Intent.PRIMARY} fill onClick={()=>{this.setState({showdialog: false}); this.props.handleAddMember(this.state.name)}}>Add</Button>
+                            </div>
+                        </div>
+                        
+                    </Card>
+                </Overlay>
+            </Card>
+        );
+    }
+}
+
 class MemberPage extends Component {
     render(){
+        const MemberList = this.props.members.map(member=>{
+            return (
+                <MemberCard key={member.id} member={member}/>
+            );
+        });
         return(
             <div className="fullview">
                 <AppBar/>
                 <div className="contentpane">
-                    <MemberCard/>
-                    <MemberCard/>
+                    {MemberList}
+                    <NewMemberCard handleAddMember={this.props.handleAddMember}/>
                 </div>
                 <BottomBar handleBackPage={this.props.handleBackPage} handleNextPage={this.props.handleNextPage}/>
             </div>
